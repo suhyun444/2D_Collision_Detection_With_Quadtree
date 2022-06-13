3a6ddc48667b0c7493061ed561094f815d28662f
정수현 <windseed71@naver.com>
2022-06-13 12:11:02 +0900

    초기 커밋.

---
+++ 2D Collision Detection With Quadtree/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "GL/glut.h"
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin
+
+	(GL_POLYGON);
+	glVertex2f(-0.2f, -0.2f);
+	glVertex2f(0.4f, -0.4f);
+	glVertex2f(0.4f, 0.4f);
+	glVertex2f(-0.2f, 0.2f);
+
+	glEnd();
+	glFinish();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("2D Collision Detection");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
