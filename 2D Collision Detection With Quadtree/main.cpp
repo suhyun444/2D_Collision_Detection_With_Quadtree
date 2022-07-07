f40a0fffbfa45759a698ab97e1fa5453b84c13e2
정수현 <windseed71@naver.com>
2022-07-07 11:40:11 +0900

    OpenGL 기본환경 구축

--- 2D Collision Detection With Quadtree/main.cpp
+++ 2D Collision Detection With Quadtree/main.cpp
@@ -1,15 +1,15 @@
 ﻿#include "GL/glut.h"
 void display() {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0f, 1.0f, 1.0f,1.0f);
+	glLoadIdentity();
+	glScaled(0.01f, 0.01f, 1.0f);
 	glColor3f(0.0f, 0.0f, 1.0f);
-	glBegin
-
-	(GL_POLYGON);
-	glVertex2f(-0.2f, -0.2f);
-	glVertex2f(0.4f, -0.4f);
-	glVertex2f(0.4f, 0.4f);
-	glVertex2f(-0.2f, 0.2f);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
 
 	glEnd();
 	glFinish();
@@ -18,8 +18,10 @@ void display() {
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
+	glutInitWindowSize(600, 600);
 	glutCreateWindow("2D Collision Detection");
 	glutDisplayFunc(display);
+	glutIdleFunc(display);
 	glutMainLoop();
 	return 0;
 }
